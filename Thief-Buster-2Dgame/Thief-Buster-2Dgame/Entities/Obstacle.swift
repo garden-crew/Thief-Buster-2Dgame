8b5b698c8507a66e775e9808f32c9845783f7444
Ilham Wisnu <ilhamwisnu42@gmail.com>
2025-07-14 15:41:23 +0700

    feat: add entities

---
+++ Thief-Buster-2Dgame/Thief-Buster-2Dgame/Entities/Obstacle.swift
@@ -0,0 +1,53 @@
+//
+//  Obstacle.swift
+//  Thief-Buster-2Dgame
+//
+//  Created by Ilham Wisnu on 14/07/25.
+//
+
+
+import Foundation
+import SpriteKit
+
+class Obstacle: SKSpriteNode {
+
+    var walkTextures: [SKTexture] {
+        (1...3).map { SKTexture(imageNamed: "ObstacleWalk\($0)") }
+    }
+
+    var walkTimePerFrame: TimeInterval { 0.2 }
+
+    init(initialTexture: String = "ObstacleWalk1", width: CGFloat) {
+        let texture = SKTexture(imageNamed: initialTexture)
+
+        let originalSize = texture.size()
+
+        let aspectRatio = originalSize.height / originalSize.width
+        
+        let height = width * aspectRatio
+
+        let scaledSize = CGSize(width: width, height: height)
+
+        super.init(texture: texture, color: .clear, size: scaledSize)
+
+        setupWalkAnimaton()
+    }
+
+    func setupWalkAnimaton() {
+        let walk = SKAction.animate(
+            with: walkTextures,
+            timePerFrame: walkTimePerFrame
+        )
+        run(.repeatForever(walk))
+    }
+    
+    func die() {
+        removeAllActions()
+        run(SKAction.removeFromParent())
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
--- Thief-Buster-2Dgame/Thief-Buster-2Dgame/Game/GameScene.swift
+++ Thief-Buster-2Dgame/Thief-Buster-2Dgame/Game/GameScene.swift
@@ -1,8 +1,71 @@
 //
-//  GameScene.swift
-//  Thief-Buster-2Dgame
+//  MyGameScene.swift
+//  TryEnemiesRandomGeneration
 //
-//  Created by Niken Larasati on 10/07/25.
+//  Created by Ilham Wisnu on 10/07/25.
 //
 
-// GameScene.swift – file utama SpriteKit yang mengatur tampilan dan update setiap frame.
+import SpriteKit
+import SwiftUI
+
+class MyGameScene: SKScene {
+
+    lazy var spawnManager: SpawnManager = SpawnManager(
+        scene: self
+    )
+
+    override func didMove(to view: SKView) {
+        backgroundColor = .black
+
+        spawnManager.generate()
+
+        let buttonTexture = SKTexture(imageNamed: "play_button")  // or use nil for solid color
+        let button = SKButton(
+            texture: buttonTexture,
+            size: CGSize(width: 100, height: 50)
+        ) {
+            self.isPaused.toggle()
+        }
+        button.position = CGPoint(x: size.width / 2, y: size.height / 2)
+
+        addChild(button)
+    }
+
+    override func willMove(from view: SKView) {
+    }
+
+}
+
+class SKButton: SKSpriteNode {
+    var action: (() -> Void)?
+
+    init(texture: SKTexture?, size: CGSize, action: (() -> Void)? = nil) {
+        super.init(texture: texture, color: .clear, size: size)
+        self.isUserInteractionEnabled = true
+        self.action = action
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.alpha = 0.7  // visual feedback
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.alpha = 1.0
+        action?()
+    }
+
+    override func touchesCancelled(
+        _ touches: Set<UITouch>,
+        with event: UIEvent?
+    ) {
+        self.alpha = 1.0
+    }
+}
+
+#Preview {
+    ContentView()
+}
